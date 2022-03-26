70c40c92ed922e20ecfaa81f009428eb239f1aa3
Rishabh Pathak <clusmsyknight@gmail.com>
2022-03-26 16:17:55 +0530

    Used ImageSelector on main component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Header } from 'semantic-ui-react';
+import ImageSelector from '../Components/ImageSelector';
 
 export default function Home() {
+  const [base64, setBase64] = useState('');
+
   return (
     <Container style={{ padding: '12px 0' }}>
       <Header size="huge" style={{ borderBottom: '1px solid #000009' }}>
         React Image Watermark
       </Header>
+      <div>
+        <ImageSelector base64={base64} setBase64={setBase64} />
+      </div>
     </Container>
   );
 }
