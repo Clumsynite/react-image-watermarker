273cee12614e29815a4aeb5cc5dc7fe2cd172176
Rishabh Pathak <clusmsyknight@gmail.com>
2022-03-26 16:56:48 +0530

    updated ImageSelector to remove or replace image

--- src/Components/ImageSelector.jsx
+++ src/Components/ImageSelector.jsx
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { string, func } from 'prop-types';
 import React, { useState, useEffect } from 'react';
-import { Image, Message } from 'semantic-ui-react';
+import { Grid, Image, Message } from 'semantic-ui-react';
 
 import { PlaceholderImage } from '../assets/images';
 import { beforeUpload, getBase64 } from '../helper/fucntions';
@@ -41,7 +41,43 @@ export default function ImageSelector({ base64, setBase64 }) {
   return (
     <>
       {base64 ? (
-        <Image src={base64} size="medium" centered label={selectedFile.name} />
+        <Grid>
+          <Grid.Row textAlign="center">
+            <Image
+              src={base64}
+              size="huge"
+              label={selectedFile.name}
+              centered
+            />
+          </Grid.Row>
+          <Grid.Row verticalAlign="middle" centered>
+            <Grid.Column textAlign="center" width={8}>
+              <button
+                type="button"
+                style={{
+                  backgroundColor: '#fff',
+                  color: '#000',
+                  padding: 8,
+                  fontSize: 16,
+                  border: '1px solid #e2e3e4',
+                  cursor: 'pointer',
+                  fontWeight: 'normal',
+                  margin: 12,
+                }}
+                onClick={() => setBase64('')}
+              >
+                Remove Image
+              </button>
+            </Grid.Column>
+            <Grid.Column textAlign="center" width={8}>
+              <UploadButton
+                label="Change Image"
+                onChange={handleChange}
+                onBeforeInput={beforeUpload}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       ) : (
         <div
           style={{
@@ -51,37 +87,12 @@ export default function ImageSelector({ base64, setBase64 }) {
             alignItems: 'center',
           }}
         >
-          <Image src={PlaceholderImage} size="medium" disabled centered />
-          <label
-            htmlFor="image-upload"
-            style={{
-              backgroundColor: '#fff',
-              color: '#000',
-              padding: 8,
-              fontSize: 16,
-              border: '1px solid #e2e3e4',
-              cursor: 'pointer',
-              fontWeight: 'normal',
-              margin: 12,
-            }}
-          >
-            <span
-              style={{
-                padding: '0 5px',
-              }}
-            >
-              Select an Image
-            </span>
-            <input
-              type="file"
-              name="myImage"
-              accept="image/png, image/gif, image/jpeg"
-              id="image-upload"
-              style={{ display: 'none' }}
-              onChange={handleChange}
-              onBeforeInput={beforeUpload}
-            />
-          </label>
+          <Image src={PlaceholderImage} size="large" disabled centered />
+          <UploadButton
+            label="Select an Image"
+            onChange={handleChange}
+            onBeforeInput={beforeUpload}
+          />
         </div>
       )}
       {error && <Message color="red">{error}</Message>}
@@ -95,4 +106,47 @@ ImageSelector.propTypes = {
 ImageSelector.defaultProps = {
   base64: '',
   setBase64: () => {},
+};
+
+function UploadButton({ label, onChange, onBeforeInput }) {
+  return (
+    <label
+      htmlFor="image-upload"
+      style={{
+        backgroundColor: '#fff',
+        color: '#000',
+        padding: 8,
+        fontSize: 16,
+        border: '1px solid #e2e3e4',
+        cursor: 'pointer',
+        fontWeight: 'normal',
+        margin: 12,
+      }}
+    >
+      <span
+        style={{
+          padding: '0 5px',
+        }}
+      >
+        {label}
+      </span>
+      <input
+        type="file"
+        name="myImage"
+        accept="image/png, image/gif, image/jpeg"
+        id="image-upload"
+        style={{ display: 'none' }}
+        onChange={onChange}
+        onBeforeInput={onBeforeInput || null}
+      />
+    </label>
+  );
+}
+UploadButton.propTypes = {
+  label: string.isRequired,
+  onChange: func.isRequired,
+  onBeforeInput: func,
+};
+UploadButton.defaultProps = {
+  onBeforeInput: null,
 };
