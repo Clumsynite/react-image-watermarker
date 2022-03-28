88a018ae43eb59d981e7943041685c8c2dc06b33
Rishabh Pathak <clusmsyknight@gmail.com>
2022-03-28 16:43:55 +0530

    fixed title

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -212,7 +212,7 @@ export default function Home() {
   return (
     <Container style={{ padding: '12px 0' }}>
       <Header size="huge" style={{ borderBottom: '1px solid #000009' }}>
-        React Image Watermark
+        React Image Watermarker
       </Header>
       <div />
       <Segment style={{ padding: '12px 0' }}>
